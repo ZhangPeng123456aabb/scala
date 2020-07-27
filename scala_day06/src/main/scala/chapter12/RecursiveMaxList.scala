70a1c8b481a1d14a8109403fc294d8c86a5db223
zhangming <2281306880@qq.com>
2020-07-27 10:33:14 +0800

    scala中用递归求最大值的方式

---
+++ scala_day06/src/main/scala/chapter12/RecursiveMaxList.scala
@@ -0,0 +1,20 @@
+package chapter12
+
+object RecursiveMaxList {
+  def main(args: Array[String]): Unit = {
+    println(List(1,-1,999,9,100).max)
+  }
+
+  /**
+    * 使用递归求List中的最大元素
+    */
+  def max(xs:List[Int]):Int = {
+    //如果为empty,抛出异常
+    if(xs.isEmpty)
+      throw new java.util.NoSuchElementException
+    if(xs.size == 1)//如果有一个元素，就是它
+      xs.head
+    //递归告诉计算机做什么，而不是告诉计算机怎么做(迭代)
+    else if (xs.head > max(xs.tail)) xs.head else max(xs.tail)
+  }
+}
